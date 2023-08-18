cbb0c6321f13fdbcf1cd4cf7b75aa11d82eba092
Anshul <anshulporwal2001@gmail.com>
2023-08-18 18:27:56 +0530

    initial commit after adding class based component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
