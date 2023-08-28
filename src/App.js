778d0f993ea7630493bb522b7c03c27d5f63ddb0
Anshul <anshulporwal2001@gmail.com>
2023-08-28 16:21:28 +0530

    No change in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 
+
 export default class App extends Component {
   render() {
     return (
