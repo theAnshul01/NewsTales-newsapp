8adcccb7081ee1799c8d2b16343347c4706cc848
Anshul <anshulporwal2001@gmail.com>
2023-08-28 15:08:10 +0530

    added functionality to the previous and next buttons

--- src/components/News.js
+++ src/components/News.js
@@ -3,12 +3,37 @@ import NewsItem from './NewsItem'
 
 export class News extends Component {
 
-  handlePrev = ()=>{
-    console.log("prev");
+  handlePrev = async ()=>{
+    // console.log("prev");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6ca81a99da1f4ad79900f12f37135c65&page=${this.state.page -1}&pageSize=9`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    this.setState({
+      page : this.state.page-1,
+      articles : parsedData.articles
+    })
+
   }
 
-  handleNext = ()=>{
-    console.log("next");
+  handleNext = async ()=>{
+    // console.log("next");
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/9)){
+        // nothing to do
+        alert("That's all for now. Thank you!");
+    }else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=6ca81a99da1f4ad79900f12f37135c65&page=${this.state.page + 1}&pageSize=9`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+
+      this.setState({
+      page : this.state.page + 1,
+      articles : parsedData.articles
+      })
+    }
+    
   }
 
   constructor(){
@@ -17,22 +42,22 @@ export class News extends Component {
     this.state = {
       articles : [],
       loading: false,
-      
+      page : 1, 
     }
   }
 
   async componentDidMount(){
     // console.log("cdm");
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=6ca81a99da1f4ad79900f12f37135c65";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=6ca81a99da1f4ad79900f12f37135c65&pageSize=9";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles : parsedData.articles});
+    this.setState({articles : parsedData.articles, totalResults : parsedData.totalResults});
   }
   render() {
     return (
       <>
-      <div className='container my-3' >
+      <div className='container my-3 p-2' >
         <h1>NewsTales - Top Headlines</h1>
         <div className="row my-4">
           {this.state.articles.map((e)=>{
@@ -40,14 +65,13 @@ export class News extends Component {
                 <NewsItem title={e.title?(e.title.length>45 ? e.title.slice(0,45): e.title):""} description = {e.description?(e.description.slice(0,80)):"In a significant medical breakthrough, a team of researchers has uncovered a potential treatment for the notorious common cold virus. The virus, responsible for countless sick days each year, has long eluded effective treatment options. However, the newly discovered compound, named \"RhinovirEx,\" has shown remarkable efficacy in early-stage clinical trials.".slice(0,45)} imageUrl={e.urlToImage?e.urlToImage:"https://timesofindia.indiatimes.com/photo/msid-103054109,imgsize-94406.cms"} newsUrl={e.url}/>
             </div>)
           })}
-            
-            
+        </div>
+        <div className="container d-flex justify-content-between my-3">
+          <button disabled={this.state.page <= 1} type="button" class="btn btn-primary" onClick={this.handlePrev}>&larr; Previous</button>
+          <button type="button" class="btn btn-primary" onClick={this.handleNext}>Next &rarr;</button>
         </div>
       </div>
-      <div className="container d-flex justify-content-between my-3">
-          <button type="button" class="btn btn-primary" onClick={this.handlePrev}>&larr; Previous</button>
-          <button type="button" class="btn btn-primary" onClick={this.handleNext}>Next &rarr;</button>
-      </div>
+      
       </>
     )
   }
